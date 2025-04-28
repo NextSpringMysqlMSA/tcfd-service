909a25f6ef09df0ea73201191129262859efb945
donghwan <kdhe9797@naver.com>
2025-04-28 20:58:24 +0900

    feat: 세팅

---
+++ src/main/java/com/nsmm/esg/tcfdservice/TcfdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.tcfdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcfdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcfdServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsmm/esg/tcfdservice/TcfdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsmm.esg.tcfdservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcfdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
